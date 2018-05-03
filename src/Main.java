c635c6bff064f0b9563467fb5097bfde0b3db7a8
kouzapo <36423445+kouzapo@users.noreply.github.com>
2018-05-03 23:08:01 +0300

    variable name fixes

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
+import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
@@ -16,20 +18,21 @@ public class Main
 		ArrayList<Order> orders = new ArrayList<Order>();
 
 		db.connect();
-		
-		//new NewCustomerFrame();
+	
 		new SellerMainFrame();
+		
+		//new ClientInfoFrame("555555555");
 		
 		/*try
 		{	
-			String query = "SELECT DISTINCT last_edit, customer_id, employee_username, closed\r\n" + 
+			String query = "SELECT DISTINCT lastEdit, clientId, employeeUsername, closed\r\n" + 
 					"FROM orders\r\n" + 
-					"WHERE closed = '0' ORDER BY last_edit";
+					"WHERE closed = '0' ORDER BY lastEdit";
 			
 			ResultSet rs = db.getStatement().executeQuery(query);
 			
 			while(rs.next())
-				orders.add(new Order(rs.getString("last_edit"), rs.getString("customer_id"), rs.getString("employee_username"), rs.getString("closed")));
+				orders.add(new Order(rs.getString("lastEdit"), rs.getString("customerId"), rs.getString("employeeUsername"), rs.getString("closed")));
 		}
 		catch (Exception ex)
 		{
@@ -39,21 +42,21 @@ public class Main
 		try
 		{
 			//Return products.
-			String query = "SELECT last_edit, product.id AS product_id,\r\n" + 
-					"quantity_weight,\r\n" + 
+			String query = "SELECT lastEdit, product.id AS productId,\r\n" + 
+					"quantityWeight,\r\n" + 
 					"orders.price\r\n" + 
-					"FROM customer, product, orders\r\n" + 
-					"WHERE orders.customer_id = customer.id\r\n" + 
-					"AND orders.product_id = product.id\r\n" +
-					"AND closed = '0' ORDER BY last_edit";
+					"FROM client, product, orders\r\n" + 
+					"WHERE orders.clientId = client.id\r\n" + 
+					"AND orders.productId = product.id\r\n" +
+					"AND closed = '0' ORDER BY lastEdit";
 			
 			ResultSet rs = db.getStatement().executeQuery(query);
 			
 			for(Order order: orders)
 			{
 				while(rs.next())
-					if(order.getLast_edit().equals(rs.getString("last_edit")))
-						order.getProducts().add(new Product(rs.getString("product_id"), rs.getString("quantity_weight"), rs.getString("orders.price")));
+					if(order.getLastEdit().equals(rs.getString("lastEdit")))
+						order.getProducts().add(new Product(rs.getString("productId"), rs.getString("quantityWeight"), rs.getString("orders.price")));
 				
 				rs.beforeFirst();
 			}
@@ -68,7 +71,7 @@ public class Main
 		
 		for(Order order: orders)
 		{
-			System.out.println(order.getCustomer_name() + " " + order.getLast_edit());
+			System.out.println(order.getClientName() + " " + order.getLastEdit());
 			
 			for(Product product: order.getProducts())
 				System.out.println(product.getName());
@@ -85,27 +88,27 @@ public class Main
 				{
 					int oldLength = orders.size();
 					
-					String query = "SELECT DISTINCT MAX(last_edit) FROM orders";
+					String query = "SELECT DISTINCT MAX(lastEdit) FROM orders";
 					ResultSet rs = db.getStatement().executeQuery(query);
 					
 					rs.next();
 					
-					String lastEdit = rs.getString("MAX(last_edit)");
+					String lastEdit = rs.getString("MAX(lastEdit)");
 					
 					if (!lastEdit.equals(oldEdit))
 					{
 						try
 						{
-							query = "SELECT DISTINCT last_edit, customer_id, employee_username, closed\r\n" + 
+							query = "SELECT DISTINCT lastEdit, clientId, employeeUsername, closed\r\n" + 
 									"FROM orders\r\n" + 
 									"WHERE closed = '0'\r\n" + 
-									"AND last_edit >= '" + lastEdit + "'\r\n" +
-									"ORDER BY last_edit";
+									"AND lastEdit >= '" + lastEdit + "'\r\n" +
+									"ORDER BY lastEdit";
 							
 							ResultSet rs1 = db.getStatement().executeQuery(query);
 							
 							while(rs1.next())
-								orders.add(new Order(rs1.getString("last_edit"), rs1.getString("customer_id"), rs1.getString("employee_username"), rs1.getString("closed")));
+								orders.add(new Order(rs1.getString("lastEdit"), rs1.getString("clientId"), rs1.getString("employeeUsername"), rs1.getString("closed")));
 						}
 						catch (Exception ex)
 						{
@@ -114,21 +117,21 @@ public class Main
 						
 						try 
 						{
-							query = "SELECT last_edit, product.id AS product_id,\r\n" + 
-									"quantity_weight,\r\n" + 
+							query = "SELECT lastEdit, product.id AS productId,\r\n" + 
+									"quantityWeight,\r\n" + 
 									"orders.price\r\n" + 
-									"FROM customer, product, orders\r\n" + 
-									"WHERE orders.customer_id = customer.id\r\n" + 
-									"AND orders.product_id = product.id\r\n" +
-									"AND last_edit >= '" + lastEdit + "' ORDER BY last_edit";
+									"FROM client, product, orders\r\n" + 
+									"WHERE orders.clientId = client.id\r\n" + 
+									"AND orders.productId = product.id\r\n" +
+									"AND last_edit >= '" + lastEdit + "' ORDER BY lastEdit";
 							
 							ResultSet rs2 = db.getStatement().executeQuery(query);
 							
 							for(int i = oldLength; i < orders.size(); i++)
 							{
 								while(rs2.next())
-									if(orders.get(i).getLast_edit().equals(rs2.getString("last_edit")))
-										orders.get(i).getProducts().add(new Product(rs2.getString("product_id"), rs2.getString("quantity_weight"), rs2.getString("orders.price")));
+									if(orders.get(i).getLastEdit().equals(rs2.getString("lastEdit")))
+										orders.get(i).getProducts().add(new Product(rs2.getString("productId"), rs2.getString("quantityWeight"), rs2.getString("orders.price")));
 								
 								rs2.beforeFirst();
 							}
@@ -142,7 +145,7 @@ public class Main
 						
 						for(int i = oldLength; i < orders.size(); i++)
 						{
-							System.out.println(orders.get(i).getCustomer_name() + " " + orders.get(i).getLast_edit());
+							System.out.println(orders.get(i).getClientName() + " " + orders.get(i).getLastEdit());
 							
 							for(Product product: orders.get(i).getProducts())
 								System.out.println(product.getName());
