e8272441df0c52adad1c90b73409f2aa231f300e
kouzapo <36423445+kouzapo@users.noreply.github.com>
2018-05-03 14:29:45 +0300

    Initial Commit

--- src/ClientsTableModel.java
+++ src/ClientsTableModel.java
@@ -2,107 +2,121 @@ import java.util.ArrayList;
 
 import javax.swing.table.AbstractTableModel;
 
-public class ClientsTableModel extends AbstractTableModel {
+public class ClientsTableModel extends AbstractTableModel
+{
+	private boolean DEBUG = true;
+	String[] clientColumnNames = {"Ονοματεπώνυμο", "ΑΦΜ", "Περιοχή", "Διεύθυνση", "Τ.Κ.", "Τηλέφωνο", "ΦΑΞ", "E-mail", "Σημειώσεις"};
+	private static ArrayList<Client> clients = new ArrayList<>();
 	
-		public ClientsTableModel(ArrayList<Product> clients) {
-			this.clients = clients;
-		}
-
-		String[] clientColumnNames = {"Ονοματεπώνυμο", "ΑΦΜ", "Περιοχή", "Διεύθυνση", "Τ.Κ.", "Τηλέφωνο", "ΦΑΞ", "E-mail", "Σημειώσεις"};
-        
-		private ArrayList<Product> clients = new ArrayList<>();
-
-        public int getColumnCount() {
-            return 9;
-        }
-
-        public int getRowCount() {
-            return clients.size();
-        }
-
-        public String getColumnName(int col) {
-            return clientColumnNames[col];
-        }
-
-        public Object getValueAt(int row, int col) {
-        	switch (col) {
-			case 0:
-				return clients.get(row).getName();
-			case 1:
-				return clients.get(row).getLocation();
-			case 2:
-				return clients.get(row).getSupplier();
-			case 3:
-				return clients.get(row).getQuality();
-			case 4:
-				return clients.get(row).getPackaging();
-			case 5:
-				return clients.get(row).getPrice();
-			case 6:
-				return clients.get(row).getStock();
-			default:
-				return "ERROR";
-			}
-        }
-        
-        public void getProductInfo(int row) {
-        	new ProductInfoFrame(clients.get(row).getId());
-        }
-
-        /*
-         * JTable uses this method to determine the default renderer/
-         * editor for each cell.  If we didn't implement this method,
-         * then the last column would contain text ("true"/"false"),
-         * rather than a check box.
-         */
-        public Class getColumnClass(int c) {
-            return getValueAt(0, c).getClass();
-        }
-
-        /*
-         * Don't need to implement this method unless your table's
-         * editable.
-         */
-        public boolean isCellEditable(int row, int col) {
-            //Note that the clients/cell address is constant,
-            //no matter where the cell appears onscreen.
-                return false;
-        }
-        
-        public void removeProductByID(String id) {
-        	if (!clients.isEmpty()) {
-				for (Product client : clients) {
-					if (client.getId().equals(id))
-						clients.remove(client);
-				}
-        	}
-        	
-			//INSERT DELETION FROM DATABASE CODE HERE
+	public ClientsTableModel(ArrayList<Client> clients)
+	{
+		ClientsTableModel.clients = clients;
+	}
+	
+	public int getColumnCount()
+    {
+        return 9;
+    }
+	
+	public int getRowCount()
+    {
+        return clients.size();
+    }
+	
+	public String getColumnName(int col)
+    {
+        return clientColumnNames[col];
+    }
+	
+	public Object getValueAt(int row, int col)
+    {
+    	switch (col)
+    	{
+    	case 0:
+    		return clients.get(row).getName();
+    	case 1:
+    		return clients.get(row).getId();
+    	case 2:
+    		return clients.get(row).getCity();
+    	case 3:
+    		return clients.get(row).getAddress();
+    	case 4:
+    		return clients.get(row).getZipCode();
+    	case 5:
+    		return clients.get(row).getPhoneNumber();
+    	case 6:
+    		return clients.get(row).getFax();
+    	case 7:
+    		return clients.get(row).getEmail();
+    	case 8:
+    		return clients.get(row).getNotes();
+    	default:
+    		return "ERROR.";
+    	}
+    }
+	
+	public static void getClientInfo(int row)
+    {
+    	new ClientInfoFrame(clients.get(row).getId());
+    }
+	
+	 /*
+     * JTable uses this method to determine the default renderer/
+     * editor for each cell.  If we didn't implement this method,
+     * then the last column would contain text ("true"/"false"),
+     * rather than a check box.
+     */
+	public Class getColumnClass(int c)
+    {
+        return getValueAt(0, c).getClass();
+    }
+	
+	/*
+     * Don't need to implement this method unless your table's
+     * editable.
+     */
+	public boolean isCellEditable(int row, int col) 
+    {
+    	//Note that the products/cell address is constant,
+    	//no matter where the cell appears onscreen.
+    	return false;
+    }
+	
+	public void removeClientByID(String id)
+    {
+    	for (Client client: clients)
+    	{
+    		if (client.getId().equals(id))
+    			clients.remove(client);
+    	}
 					
-			//not sure if array list adds the item last
-			fireTableDataChanged(); //to be changed, not efficient
-		}
-        
-        public void removeSelectedProduct(int row) {
-        	if (!clients.isEmpty())
-        		clients.remove(clients.get(row));
-			
-			//INSERT DELETION FROM DATABASE CODE HERE
-        	
-			fireTableDataChanged(); //to be changed, not efficient
-		}
-        
-        //alternative way to add a row
-        public void addRow(Product client) {
-        	int lastRowBeforeUpdate = this.getRowCount() - 1;
-        	clients.add(client);
-        	int lastRowAfterUpdate = this.getRowCount() - 1;
-        	fireTableRowsInserted(lastRowBeforeUpdate, lastRowAfterUpdate);
-        }
-        
-        //this updates the table model, using a data structure and informs the table that it must refresh it's contents
-		public void update(ArrayList<Product> clients) {
-			this.clients = clients;
-			fireTableDataChanged();
-		}
+		//not sure if array list adds the item last
+		fireTableDataChanged(); //to be changed, not efficient
+	}
+	
+	public void removeSelectedClient(int row)
+    {
+    	if (!clients.isEmpty())
+    		clients.remove(clients.get(row));
+		
+		//INSERT DELETION FROM DATABASE CODE HERE
+    	
+		fireTableDataChanged(); //to be changed, not efficient
+	}
+	
+	//alternative way to add a row
+    public void addRow(Client client)
+    {
+    	int lastRowBeforeUpdate = this.getRowCount() - 1;
+    	clients.add(client);
+    	int lastRowAfterUpdate = this.getRowCount() - 1;
+    	fireTableRowsInserted(lastRowBeforeUpdate, lastRowAfterUpdate);
+    }
+    
+    //this updates the table model, using a data structure and informs the table that it must refresh it's contents
+	public void update(ArrayList<Client> clients)
+	{
+		this.clients = clients;
+		fireTableDataChanged();
+	}
 }
