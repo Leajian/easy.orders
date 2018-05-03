de7a8b8dee153a7b39f3c6a8a203ce42a6f7e05b
kouzapo <36423445+kouzapo@users.noreply.github.com>
2018-05-03 14:30:43 +0300

    Initial Commit

---
+++ src/Client.java
@@ -0,0 +1,172 @@
+import java.sql.ResultSet;
+
+public class Client
+{
+	private String id;
+	
+	private DBConnect db = new DBConnect();
+	
+	public Client(String id)
+	{
+		db.connect();
+		
+		this.id = id;
+	}
+	
+	public String getId()
+	{
+		return id;
+	}
+	
+	public String getName()
+	{
+		try
+		{
+			String query = "SELECT name FROM customer WHERE id = " + id;
+			ResultSet rs = db.getStatement().executeQuery(query);
+			
+			rs.next();
+			
+			return rs.getString("name");
+		}
+		catch (Exception ex)
+		{
+			ex.printStackTrace();
+		}
+		
+		return "";
+	}
+	
+	public String getCity()
+	{
+		try
+		{
+			String query = "SELECT city FROM customer WHERE id = " + id;
+			ResultSet rs = db.getStatement().executeQuery(query);
+			
+			rs.next();
+			
+			return rs.getString("city");
+		}
+		catch (Exception ex)
+		{
+			ex.printStackTrace();
+		}
+		
+		return "";
+	}
+	
+	public String getPhoneNumber()
+	{
+		try
+		{
+			String query = "SELECT phoneNumber FROM customer WHERE id = " + id;
+			ResultSet rs = db.getStatement().executeQuery(query);
+			
+			rs.next();
+			
+			return rs.getString("phoneNumber");
+		}
+		catch (Exception ex)
+		{
+			ex.printStackTrace();
+		}
+		
+		return "";
+	}
+	
+	public String getEmail()
+	{
+		try
+		{
+			String query = "SELECT email FROM customer WHERE id = " + id;
+			ResultSet rs = db.getStatement().executeQuery(query);
+			
+			rs.next();
+			
+			return rs.getString("email");
+		}
+		catch (Exception ex)
+		{
+			ex.printStackTrace();
+		}
+		
+		return "";
+	}
+	
+	public String getAddress()
+	{
+		try
+		{
+			String query = "SELECT address FROM customer WHERE id = " + id;
+			ResultSet rs = db.getStatement().executeQuery(query);
+			
+			rs.next();
+			
+			return rs.getString("address");
+		}
+		catch (Exception ex)
+		{
+			ex.printStackTrace();
+		}
+		
+		return "";
+	}
+	
+	public String getFax()
+	{
+		try
+		{
+			String query = "SELECT fax FROM customer WHERE id = " + id;
+			ResultSet rs = db.getStatement().executeQuery(query);
+			
+			rs.next();
+			
+			return rs.getString("fax");
+		}
+		catch (Exception ex)
+		{
+			ex.printStackTrace();
+		}
+		
+		return "";
+	}
+	
+	public String getZipCode()
+	{
+		try
+		{
+			String query = "SELECT zipCode FROM customer WHERE id = " + id;
+			ResultSet rs = db.getStatement().executeQuery(query);
+			
+			rs.next();
+			
+			return rs.getString("zipCode");
+		}
+		catch (Exception ex)
+		{
+			ex.printStackTrace();
+		}
+		
+		return "";	
+	}
+	
+	public String getNotes()
+	{
+		try
+		{
+			String query = "SELECT notes FROM customer WHERE id = " + id;
+			ResultSet rs = db.getStatement().executeQuery(query);
+			
+			rs.next();
+			
+			return rs.getString("notes");
+		}
+		catch (Exception ex)
+		{
+			ex.printStackTrace();
+		}
+		
+		return "";
+	}
+}
