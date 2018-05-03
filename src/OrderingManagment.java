4151700242e367c5cb8c919d3a502028e6cbe3d6
kouzapo <36423445+kouzapo@users.noreply.github.com>
2018-05-04 00:03:51 +0300

    fetchOrdersRecord added

--- src/OrderingManagment.java
+++ src/OrderingManagment.java
@@ -53,4 +53,27 @@ public class OrderingManagment
 		
 		return clients;
 	}
+	
+	public static ArrayList<Order> fetchRecord()
+	{
+		ArrayList<Order> ordersRecord = new ArrayList<>();
+		DBConnect db = new DBConnect();
+		
+		db.connect();
+		
+		try
+		{
+			String query = "SELECT DISTINCT lastEdit, clientId, employeeUsername, closed FROM orders ORDER BY lastEdit";
+			ResultSet rs = db.getStatement().executeQuery(query);
+			
+			while(rs.next())
+				ordersRecord.add(new Order(rs.getString("lastEdit"), rs.getString("clientId"), rs.getString("employeeUsername"), rs.getString("closed")));
+		} 
+		catch (Exception ex)
+		{
+			ex.printStackTrace();
+		}
+		
+		return ordersRecord;
+	}
 }
